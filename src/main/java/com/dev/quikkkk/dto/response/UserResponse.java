1ffae78351324f727201af195edcb4a475f0b22f
quikkkk <kexitttttt@gmail.com>
2025-09-01 22:05:17 +0300

    User response

---
+++ src/main/java/com/dev/quikkkk/dto/response/UserResponse.java
@@ -0,0 +1,18 @@
+package com.dev.quikkkk.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserResponse {
+    private String id;
+    private String username;
+    private String email;
+}
