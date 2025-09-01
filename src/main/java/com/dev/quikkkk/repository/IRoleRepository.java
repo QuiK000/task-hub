5ebc1958319bbca87bce07a35a17c35a45048314
quikkkk <kexitttttt@gmail.com>
2025-09-01 21:06:01 +0300

    Added annotation in the role repository

--- src/main/java/com/dev/quikkkk/repository/IRoleRepository.java
+++ src/main/java/com/dev/quikkkk/repository/IRoleRepository.java
@@ -2,9 +2,11 @@ package com.dev.quikkkk.repository;
 
 import com.dev.quikkkk.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface IRoleRepository extends JpaRepository<Role, String> {
     Optional<Role> findByName(String roleName);
 }
