5316c6265227ff315ba81cec61ff1490a96e2182
quikkkk <kexitttttt@gmail.com>
2025-09-01 22:00:47 +0300

    refresh token request

---
+++ src/main/java/com/dev/quikkkk/dto/request/RefreshRequest.java
@@ -0,0 +1,16 @@
+package com.dev.quikkkk.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RefreshRequest {
+    private String refreshToken;
+}
