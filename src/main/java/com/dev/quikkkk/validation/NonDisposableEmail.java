dc1307cffa7d2fa921b62d82999114702a888f2a
quikkkk <kexitttttt@gmail.com>
2025-09-01 21:55:43 +0300

    Email domain validator

---
+++ src/main/java/com/dev/quikkkk/validation/NonDisposableEmail.java
@@ -0,0 +1,19 @@
+package com.dev.quikkkk.validation;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD, ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = EmailDomainValidator.class)
+public @interface NonDisposableEmail {
+    String message() default "Disposable email address are not allowed.";
+
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
