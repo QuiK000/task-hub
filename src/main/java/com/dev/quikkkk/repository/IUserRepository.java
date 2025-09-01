5861f71ed774aab22bd31fef97f42fbc90e540da
quikkkk <kexitttttt@gmail.com>
2025-09-01 22:01:29 +0300

    User repository

---
+++ src/main/java/com/dev/quikkkk/repository/IUserRepository.java
@@ -0,0 +1,9 @@
+package com.dev.quikkkk.repository;
+
+import com.dev.quikkkk.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, String> {
+}
