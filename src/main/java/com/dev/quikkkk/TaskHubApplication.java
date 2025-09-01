730994c9d2fc75eb6a2da98921b5e26f1469407e
quikkkk <kexitttttt@gmail.com>
2025-09-01 19:31:28 +0300

    Init project

---
+++ src/main/java/com/dev/quikkkk/TaskHubApplication.java
@@ -0,0 +1,13 @@
+package com.dev.quikkkk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskHubApplication.class, args);
+	}
+
+}
