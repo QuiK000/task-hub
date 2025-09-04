3492fa2501e8c4f3369fadb17a5064b6957cc8f5
quikkkk <kexitttttt@gmail.com>
2025-09-04 23:23:24 +0300

    ...

--- src/main/java/com/dev/quikkkk/repository/IUserRedisRepository.java
+++ src/main/java/com/dev/quikkkk/repository/IUserRedisRepository.java
@@ -1,6 +1,6 @@
 package com.dev.quikkkk.repository;
 
-import com.dev.quikkkk.entity.UserRedisEntity;
+import com.dev.quikkkk.entity.redis.UserRedisEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
