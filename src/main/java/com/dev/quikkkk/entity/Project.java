c3d0502de3dca57ff6d4afda0b4cc1ee196cceb2
quikkkk <kexitttttt@gmail.com>
2025-09-06 18:44:52 +0300

    add one-to-many relationship between Project and Task

--- src/main/java/com/dev/quikkkk/entity/Project.java
+++ src/main/java/com/dev/quikkkk/entity/Project.java
@@ -1,17 +1,22 @@
 package com.dev.quikkkk.entity;
 
 import com.dev.quikkkk.entity.common.BaseEntity;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "projects")
@@ -30,4 +35,9 @@ public class Project extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "owner_id", nullable = false)
     private User ownerId;
+
+    @OneToMany(mappedBy = "projectId", cascade = {
+            CascadeType.ALL
+    }, orphanRemoval = true, fetch = FetchType.LAZY)
+    private Set<Task> tasks = new HashSet<>();
 }
