197e118226e553b2d47272b3146d15dad0b70ebb
quikkkk <kexitttttt@gmail.com>
2025-09-06 20:16:40 +0300

    removed entity listeners

--- src/main/java/com/dev/quikkkk/entity/redis/UserRedisEntity.java
+++ src/main/java/com/dev/quikkkk/entity/redis/UserRedisEntity.java
@@ -1,13 +1,11 @@
 package com.dev.quikkkk.entity.redis;
 
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
@@ -19,7 +17,6 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@EntityListeners(AuditingEntityListener.class)
 public class UserRedisEntity {
     @Id
     private String id;
